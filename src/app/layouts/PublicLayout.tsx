8351164f4c5afd88b5c8a57df763e55f8fa8afde
John Marlon <montoyamarlon14@gmail.com>
2026-08-26 19:23:02 -0500

    fix(navigation): reset scroll position between pages

--- src/app/layouts/PublicLayout.tsx
+++ src/app/layouts/PublicLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router'
 import { Footer } from '@/components/layout/Footer'
 import { Header } from '@/components/layout/Header'
+import { ScrollToTop } from '@/components/layout/ScrollToTop'
 
 export function PublicLayout() {
   return (
     <div className="app-shell">
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
